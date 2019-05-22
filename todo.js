8c7908eee85487cabda24dd77f608938242c4cd5
Melissa Smith <melissasmith@Melissas-MacBook-Air.local>
2019-05-22 15:33:14 -0700

    mixing two api

--- todo.js
+++ todo.js
@@ -1,26 +1,32 @@
 fetch('https://jsonplaceholder.typicode.com/todos')
-
+//resoving the response from the promise that the fetch returned
     .then(res => res.json())
-    //this .then is converting it into data that we will than organize and manipulate 
     .then(todos => {
         entryPoint = document.getElementById('app')
         let todoList = document.createElement('ul')
-            // newTodoList.classList.add("list-group-item")
-        
-        
-        
-
-        todos.forEach(todo => {
-            let newTodoList = document.createElement('li')
-                // newTodoList.classList.add("list-group-item")
-            
-            
-            newTodoList.innerHTML = `
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(res => res.json())
+            .then(comments {
+                entryPoint = document.getElementById('app')
+                // let emailList = document.createElement('ul')
+            })
+                //it does not like my two arguments in the for loop 
+        todos.forEach((todo,email) => {
+            let todoItem = document.createElement('li')
+                todoItem.innerHTML = `
                 <p>${todo.id}</p>
                 <p>${todo.title}</p>
                 <p>${todo.completed}</p>
-            `
-            todoList.appendChild(newTodoList)
+                `
+            let emailList = document.createElement('li')
+                //below i am trying set the email to the post id's that match
+                if(todo.id[0] === email.id[0]){
+                    todoList.id[i].appendChild(emailList.email[i])
+                
+                }
+                todoList.appendChild(todoItem)
+                
+                
         })
-    entryPoint.appendChild(todoList)
+        entryPoint.appendChild(todoList)
     })
