abd5bc60edcd40a0469dbaf4e96c1756a62dbbd4
Melissa Smith <melissasmith@Melissas-MacBook-Air.local>
2019-05-22 20:01:23 -0700

    yet another attempt

--- todo.js
+++ todo.js
@@ -1,32 +1,28 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-//resoving the response from the promise that the fetch returned
+let todoList = fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json());
+
+let comments = fetch('https://jsonplaceholder.typicode.com/comments')
     .then(res => res.json())
-    .then(todos => {
-        entryPoint = document.getElementById('app')
-        let todoList = document.createElement('ul')
-        fetch('https://jsonplaceholder.typicode.com/comments')
-            .then(res => res.json())
-            .then(comments {
-                entryPoint = document.getElementById('app')
-                // let emailList = document.createElement('ul')
-            })
-                //it does not like my two arguments in the for loop 
-        todos.forEach((todo,email) => {
-            let todoItem = document.createElement('li')
-                todoItem.innerHTML = `
-                <p>${todo.id}</p>
-                <p>${todo.title}</p>
-                <p>${todo.completed}</p>
-                `
-            let emailList = document.createElement('li')
-                //below i am trying set the email to the post id's that match
-                if(todo.id[0] === email.id[0]){
-                    todoList.id[i].appendChild(emailList.email[i])
-                
-                }
-                todoList.appendChild(todoItem)
-                
-                
-        })
-        entryPoint.appendChild(todoList)
+let  entryPoint = document.getElementById('app')
+let newList = document.createElement('ul')
+
+let combinedData = {"todoList":{}, "comments":{}}
+Promise.all([todoList,comments]).then(function(values){
+    combinedData["todoList"] = values[0]
+    combinedData["comments"] = values[1]
+    return combinedData;
+    //as cobinedData is an object or so i think it is an object with arrays we can't do a forEach unless we use Object.keys i don't get an error when i do this however my concern is if iam converting to an array than combine so the .method does not work?  i tried the []as well
+    Object.keys(combinedData).forEach(data => {
+        let listItems = document.createElement('li')
+        listItems.innerHTML = `
+        <p>${values[0].id}</p>
+        <p>${values[0].title}</p
+        <p>${values[1].email}</p>
+        `
+        newList.appendChild(listItems)
     })
+entryPoint.appendChild(newList)
+    
+});
+
+
